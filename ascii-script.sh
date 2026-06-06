c0faadd429abf6d57a78d0d50d4289c17e2ec767
pradeepneosoft <56916932+pradeepneosoft@users.noreply.github.com>
2026-06-06 23:43:01 +0530

    sudo added in script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-apt-get install cowsay -y
+sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 grep -irn "dragon" dragon.txt
 cat dragon.txt
