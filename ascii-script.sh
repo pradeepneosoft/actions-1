81ad25becb9894feed275537cac815dd4c6bd3e8
pradeepneosoft <56916932+pradeepneosoft@users.noreply.github.com>
2026-06-07 18:07:05 +0530

    elephant

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f alligator "Run for cover, I am a ALLIGATOR....RAWR" >> alligator.txt
-grep -irn "alligator" alligator.txt
-cat alligator.txt
+cowsay -f elephant "Run for cover, I am an ELEPHANT....RAWR" >> elephant.txt
+grep -irn "elephant" elephant.txt
+cat elephant.txt
 ls -ltra
